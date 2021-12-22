7361522a868bd364cb23544caf15720e719a24dc
AndreiM <andreim@jukalang.com>
2021-12-22 00:35:47 -0500

    Site Framework update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   projectName: 'juka-website', // Usually your repo name.
 
   plugins: [
-    'docusaurus2-dotenv',
+    ['docusaurus2-dotenv',
     {
       path: "./.env", // The path to your environment variables.
       safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
@@ -25,7 +25,7 @@ const config = {
       silent: false, //  If true, all warnings will be suppressed
       expand: false, // Allows your variables to be "expanded" for reusability within your .env file
       defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
-    }
+    }]
   ],
   presets: [
     [
